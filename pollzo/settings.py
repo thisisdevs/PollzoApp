9833aabe9f1ee0c44a1e92abbdbcaf8ea2625680
thisisdevs <thisisdevshekhawat@gmail.com>
2017-05-09 16:15:37 +0530

    prepared files to deployement to heroku

--- pollzo/settings.py
+++ pollzo/settings.py
@@ -128,9 +128,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'userProfile/static'),
-    os.path.join(PROJECT_ROOT, 'UserSigning/static'),
-
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
