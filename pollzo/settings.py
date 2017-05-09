8401c8af5abf78fe5b09e9db118a5d659cdba2e3
thisisdevs <thisisdevshekhawat@gmail.com>
2017-05-09 15:46:32 +0530

    prepared files to deployement to heroku

--- pollzo/settings.py
+++ pollzo/settings.py
@@ -123,3 +123,17 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_URL = '/login/'
+
+mport dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
